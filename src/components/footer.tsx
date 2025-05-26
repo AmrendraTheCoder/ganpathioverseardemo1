80bbdd94c08d3a984adce2a5523809c6ecff8933
Amrendra Vikram Singh <76041208+AmrendraTheCoder@users.noreply.github.com>
2025-05-26 19:31:01 +0530

    removing circle from the footer.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -51,9 +51,7 @@ export default function Footer() {
     <footer className="bg-slate-900 text-white relative overflow-hidden">
       {/* Decorative Background Elements */}
       <div className="absolute inset-0 opacity-10">
-        <div className="absolute top-20 left-20 w-32 h-32 border border-white rounded-full"></div>
-        <div className="absolute bottom-20 right-20 w-24 h-24 border border-white rounded-full"></div>
-        <div className="absolute top-1/2 left-1/2 w-40 h-40 border border-white rounded-full transform -translate-x-1/2 -translate-y-1/2"></div>
+       
       </div>
 
       {/* Main Footer Content */}
